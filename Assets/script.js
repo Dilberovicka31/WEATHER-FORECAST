8047c2248caa21d2b61b9bd798bfff89ea870d9d
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-14 22:42:36 -0800

    initial files, still working on it

---
+++ Assets/script.js
@@ -0,0 +1,34 @@
+// Search to input the city 
+
+// WHEN the user clicks the search btn
+
+funciton handleSearch(){
+// THEN get the user input value that was entered in the search input
+}
+function makeWeaherRequest(search) {}
+
+//Reuse the function to get all the data 
+
+// NEXT we need to build the URL for the first API request, chain of API requests 
+
+//Get te current weather data information data2.5 weather 
+//Example Build hte userinput into the url
+
+// /nex MAKE THE REQ TO THE URL iwth Jquery ajax
+
+$.ajax(queryURL).then(function(response){
+    //sTART RENDERING DATA TO THE html 
+    // then get the lat and lng out of the response object
+    //Call next makeOneCallRequest (lat, lng)function and pass in the lat and lng
+
+});
+
+
+
+// How do we chain .then do the next step
+
+// functioN makeOneCallRequest (lat, lng) one call API N=MAKE ONE CALL REQ ACCEPT LATTITUDE AND LNG
+$.ajax(queryURL).then(function(response){
+
+    // finish rendering data in the HTML 
+}
