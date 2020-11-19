2e431b1db8f43ba30b5e3eb72c7f11c20eda7fe5
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-18 17:40:49 -0800

    -commented out pseudocode

--- Assets/script.js
+++ Assets/script.js
@@ -24,10 +24,10 @@ function getWeather(){
 
 // WHEN the user clicks the search btn
 
-funciton handleSearch(){
+// funciton handleSearch(){
 // THEN get the user input value that was entered in the search input
-}
-function makeWeaherRequest(search) {}
+// }
+// function makeWeaherRequest(search) {}
 
 //Reuse the function to get all the data 
 
@@ -38,19 +38,19 @@ function makeWeaherRequest(search) {}
 
 // /nex MAKE THE REQ TO THE URL iwth Jquery ajax
 
-$.ajax(queryURL).then(function(response){
+// $.ajax(queryURL).then(function(response){
     //sTART RENDERING DATA TO THE html 
     // then get the lat and lng out of the response object
     //Call next makeOneCallRequest (lat, lng)function and pass in the lat and lng
 
-});
+// });
 
 
 
 // How do we chain .then do the next step
 
 // functioN makeOneCallRequest (lat, lng) one call API N=MAKE ONE CALL REQ ACCEPT LATTITUDE AND LNG
-$.ajax(queryURL).then(function(response){
+// $.ajax(queryURL).then(function(response){
 
     // finish rendering data in the HTML 
-}
+// }
