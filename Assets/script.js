b691b8505931086876e58e6168757680f41e0ad0
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-22 13:52:59 -0800

    added 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+// var now = moment();
+// var date = ("#city-date").text(moment().format("YYYY-MM-DD"));
 var apiKey = "bb097bc0ef72343cff93dc70db113b3e";
 var searchResult;
 var searchEl = $("#searchCity");
@@ -34,23 +36,47 @@ function getWeather(searchOption){
         var citiesEl = $("<button>").text(city);
         citiesDiv.append(citiesEl);
         console.log(citiesDiv);
-       
+
+        // Display city name
+        var cityName = (response.city.name);
+        var date = (response.list[0].dt_txt);
+        var icon = (response.list[0].weather[0].icon);
+        $("#city-date").text(cityName, date, icon);
+        
+        //Temperature
         var temperatureF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
         $(".card-temp").text('Temperature: ' + temperatureF.toFixed(0) + '°F');
         console.log(temperatureF);
 
+        //Humidity
         var humidityEl =(response.list[0].main.humidity);
-        $(".card-hum").text("Humidity: " + humidityEl);
+        $(".card-hum").text("Humidity: " + humidityEl + "%");
 
+        //Wind speed
         var windSpeed = (response.list[0].wind.speed);
         $(".card-wind").text("Wind Speed: " + windSpeed);
 
-        var dayOne = (response.list[1].main.temperatureF);
-        $(".card-d1").text(dayOne);
-        console.log(dayOne);
+        //5 Day forecast - day 1
+        var tempDay1 = (response.list[1].main.temp - 273.15) * 1.80 + 32;
+        $("#day1").text('Temperature: ' + tempDay1.toFixed(0) + '°F');
 
-        // url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchOption + "&appid=" + apiKey,
-        // var uvIndex = "api.openweathermap.org/data/2.5/uvi?lat=" searchOption.coord.lat + '&lon=' + searchOption.coord.lon + '&appid=' + APIKey;
+        console.log(tempDay1);
+
+        var humDay1 = (response.list[1].main.humidity);
+        // tempDay1.text('Humidity: ' + humDay1 + "%");
+
+        var tempDay2 = (response.list[1].main.temp - 273.15) * 1.80 + 32;
+        $("#day2").text('Temperature: ' + tempDay2.toFixed(0) + '°F');
+
+        var tempDay3 = (response.list[2].main.temp - 273.15) * 1.80 + 32;
+        $("#day3").text('Temperature: ' + tempDay3.toFixed(0) + '°F');
+
+        var tempDay4 = (response.list[3].main.temp - 273.15) * 1.80 + 32;
+        $("#day4").text('Temperature: ' + tempDay4.toFixed(0) + '°F');
+
+        var tempDay5 = (response.list[4].main.temp - 273.15) * 1.80 + 32;
+        $("#day5").text('Temperature: ' + tempDay5.toFixed(0) + '°F');
+      
         
         // $.ajax({
         //     url: "api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + '&lon=' + response.coord.lon + '&appid=' + apiKey,
